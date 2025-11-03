53d07b43c390902e01483d1811f7933107b35737
13dmahon <135659148+13dmahon@users.noreply.github.com>
2025-11-03 14:44:10 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    target: 'es2015',
+    minify: 'terser',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
