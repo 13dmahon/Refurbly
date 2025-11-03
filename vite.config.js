bbe43822f3f982f38baeb53eaccda1bcd982ca68
13dmahon <135659148+13dmahon@users.noreply.github.com>
2025-11-03 15:07:14 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   base: './',
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-    }),
-  ],
+  plugins: [react()],
 })
